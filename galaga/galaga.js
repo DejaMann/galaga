070aa98f7cb2d9650ca9dac195c11b4928ab50a6
Deja Mann <110996266+DejaMann@users.noreply.github.com>
2022-09-19 23:25:30 -0400

    first commit

---
+++ galaga/galaga.js
@@ -0,0 +1,17 @@
+
+
+class Player {
+    constructor () {
+        this.name = Player 1
+        this.position {
+            
+        }
+        // this.image = 
+
+        this.width = 100;
+        this.height = 100;
+
+    }
+
+}
+
