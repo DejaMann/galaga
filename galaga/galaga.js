d725abe6432465276f3dc933fbdf66ea7cbc8cad
Deja Mann <110996266+DejaMann@users.noreply.github.com>
2022-09-21 00:00:53 -0400

    second commit - starting over

--- galaga/galaga.js
+++ galaga/galaga.js
@@ -1,17 +1,78 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+canvas.width = 600;
+canvas.height = 600;
+
+const background = new Image ();
+background.src = 
 
 
-class Player {
-    constructor () {
-        this.name = Player 1
-        this.position {
-            
-        }
-        // this.image = 
 
-        this.width = 100;
-        this.height = 100;
 
-    }
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ship = document.getElementById('ship')
+// const firstEnemy = []
+
+// ship = () => {//ship starting position, bottom middle of screen
+//     this.x = width/2
+//     this.move = function(arrow) {
+//         this.x += (arrow)
+//     }
+
+// }
+
+// firstEnemy = () => {//ship starting position, bottom middle of screen
+//     this.x = width/2
+// }
+
+
+// keyPressed = () => {
+//     if (keyCode === 'ArrowLeft') {
+//         ship.move(-1);
+//     } else if (keyCode === 'ArrowRight') {
+//         ship.move(1)
+
+// }
+
+
+
+
+
+// const secondEnemy
+// const thirdEnemy
+
 
